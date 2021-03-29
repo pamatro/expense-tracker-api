9e3f453edea6cc8ee5598dc325488204ff578f20
pamatro <pamatro@gmail.com>
2021-03-29 12:45:26 -0300

    Added API key to the views and added it in the request headers of the client used to make test requests

--- restapi/views.py
+++ restapi/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import RetrieveDestroyAPIView, ListCreateAPIView
 
 from restapi import models, serializers
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_api_key.permissions import HasAPIKey
 
 
 # Create your views here.
@@ -9,10 +10,11 @@ class ExpenseListCreate(ListCreateAPIView):
     serializer_class = serializers.Expense
     queryset = models.Expense.objects.all()
     filterset_fields = ['category', 'merchant']
-    permission_classes = [IsAuthenticated]
+    permission_classes = [HasAPIKey]
 
 
 class ExpenseRetrieveDelete(RetrieveDestroyAPIView):
     serializer_class = serializers.Expense
     queryset = models.Expense.objects.all()
+    permission_classes = [HasAPIKey]
 
